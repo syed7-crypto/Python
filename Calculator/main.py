fb91a0370607450d30da03167aa2e4a3605684bd
Syed <syed786000000@gmail.com>
2026-06-30 20:58:04 +0530

    Refactor calculator and add calculation history

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,6 @@
 import os
-print("Python Calculator")
+
+history = []
 
 def get_numbers():
     a = float(input("a: "))
@@ -12,30 +13,31 @@ def clear_screen():
     else:
         os.system("clear")
 
-def add():
-    a,b=get_numbers()
+def view_history():
+    if len(history) == 0:
+        print ("No history available.")
+    else:
+        for element in history:
+            print(element)
+
+def add(a,b):
     return a+b
 
-def subtrat():
-    a,b=get_numbers()
+def subtract(a,b):
     return a-b
 
-def multiply():
-    a,b=get_numbers()
+def multiply(a,b):
     return a*b
 
-def divide():
-    a,b=get_numbers()
+def divide(a,b):
     if b==0:
         return "Cannot divide by Zero"
     return a/b
 
-def power():
-    a,b=get_numbers()
+def power(a,b):
     return a**b
 
-def modulus():
-    a,b=get_numbers()
+def modulus(a,b):
     if b==0:
         return "Cannot divide by Zero"
     return a%b 
@@ -43,42 +45,65 @@ def modulus():
 def main():
     while True :
         clear_screen()
-
+        print("===Python Calculator===\n\n")
         print("""1. Add
 2. Subtract
 3. Multiply
 4. Divide
 5. Power
 6. Modulus
-7. Exit
+7. View History
+8. Exit
 """)
         choice = int(input("Choice: "))
 
-        if choice == 1:
-            print(f"Result: {add()}")
-            input("\nPress Enter to continue...")
-        elif choice == 2:
-            print(f"Result: {subtrat()}")
-            input("\nPress Enter to continue...")
-        elif choice == 3:
-            print(f"Result: {multiply()}")
-            input("\nPress Enter to continue...")
-        elif choice == 4:
-            print(f"Result: {divide()}")
-            input("\nPress Enter to continue...")
-        elif choice == 5:
-            print(f"Result: {power()}")
-            input("\nPress Enter to continue...")
-        elif choice == 6:
-            print(f"Result: {modulus()}")
-            input("\nPress Enter to continue...")
-        elif choice == 7:
+        if choice == 8:
             print("Goodbye!")
-            input("\nPress Enter to continue...")
+            input("\nPress Enter to exit...")
             break
-        else:
+        elif choice == 7:
+            print("Previous operations:  ")
+            view_history()
+            input("\nPress Enter to continue...")
+            continue
+        elif choice < 1 or choice > 8:
             print("Invalid Choice!")
+            input("\nPress Enter to continue...")
+            continue
+        
+
+        a,b=get_numbers()
+
+        result=0
+        operator=""
+
+        if choice == 1:
+            result=add(a,b)
+            operator="+"
+        elif choice == 2:
+            result=subtract(a,b)
+            operator="-"
+        elif choice == 3:
+            result=multiply(a,b)
+            operator="*"
+        elif choice == 4:
+            result=divide(a,b)
+            operator="/"
+        elif choice == 5:
+            result=power(a,b)
+            operator="**"
+        elif choice == 6:
+            result=modulus(a,b)
+            operator="%"
+        
+        
+        
+        print(f"Result: {result}")
+        history.append(f"{a} {operator} {b} = {result}")
+        input("\nPress Enter to continue...")
 
 
 
-main ()    
+
+
+main()    
