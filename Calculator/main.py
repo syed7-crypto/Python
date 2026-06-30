1eaae2d198f66a2d8b63c4a0961c4dd6b1188019
Syed <syed786000000@gmail.com>
2026-06-30 18:54:50 +0530

    Add menu

--- Calculator/main.py
+++ Calculator/main.py
@@ -29,3 +29,18 @@ def mod_fun():
     a=float(input("a:"))
     b=float(input("b:"))
     return a%b 
+
+def main():
+    while True :
+        print("""1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Power
+6. Modulus
+7. Exit
+""")
+        choice = input("Choice: ")
+
+
+        
