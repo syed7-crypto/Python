bd04f8862e181baeb925a1f79b7b1dec3cfe842d
Syed <syed786000000@gmail.com>
2026-06-30 18:33:52 +0530

    Implement division

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,8 +1,21 @@
 print("Python Calculator")
 
 def add_fun():
-    print("Enter two numbers:  ")
-    a=float(input())
-    b=float(input())
+    a=float(input("a:"))
+    b=float(input("b:"))
     return a+b
 
+def sub_fun():
+    a=float(input("a:"))
+    b=float(input("b:"))
+    return a-b
+
+def mul_fun():
+    a=float(input("a:"))
+    b=float(input("b:"))
+    return a*b
+
+def div_fun():
+    a=float(input("a:"))
+    b=float(input("b:"))
+    return a/b
